12b1fb36374d424de2b6e53c3b6b04d483773088
Shaad Khan <shaad39@gmail.com>
2017-07-10 15:22:00 +0530

    c

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -22,3 +22,25 @@ $scope.con=response.data;
    });
 
 })
+app.controller("mycntrl2",function($scope,$http)
+{
+    //console.log("controller called");
+    console.log("we are here");
+$scope.add_info=function()
+{
+ console.log($scope.contact);
+ $http.post('/config2',$scope.contact);
+ $http.get('/config2').then(function(response){
+$scope.con=response.data;
+
+
+   });
+
+}
+  $http.get('/config2').then(function(response){
+$scope.con=response.data;
+
+
+   });
+
+})
