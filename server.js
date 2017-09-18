7afdd8aa549e3742097c77b12bf5c192542aaeb0
Shaad Khan <shaad39@gmail.com>
2017-09-18 19:18:04 +0530

    d

--- server.js
+++ server.js
@@ -132,6 +132,30 @@ res.json(docs);
 });
     });
 });
+app.get('/testsql',function(req,res){
+    //var per=[];
+    MongoClient.connect(url, function(err, db) {
+assert.equal(null, err);
+var cursor =db.collection('testsql').find().toArray(function(err, docs)
+{
+ assert.equal(err, null);
+res.json(docs);
+
+});
+    });
+});
+app.get('/dbconfig',function(req,res){
+    //var per=[];
+    MongoClient.connect(url, function(err, db) {
+assert.equal(null, err);
+var cursor =db.collection('Dbconfig').find().toArray(function(err, docs)
+{
+ assert.equal(err, null);
+res.json(docs);
+
+});
+    });
+});
 
 app.get('/cpu',function(req,res){
     //var per=[];
